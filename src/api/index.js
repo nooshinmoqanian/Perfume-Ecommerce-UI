4601fd76e57dcd28603a35af1a1bd2f5cae4d06a
nooshin moghanian <nooshinmoqanian@gmail.com>
2026-05-28 14:08:03 +0330

    chore(frontend): remove legacy JavaScript source files

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./config";
-export * from "./http";
-export * from "./products.api";
-export * from "./orders.api";
-export * from "./auth.api";
-export * from "./notifications.api";
--- src/hooks/useToast.js
+++
@@ -1,7 +0,0 @@
-export const showToast = (message, tone = "info") => {
-  try {
-    window.dispatchEvent(new CustomEvent("app-alert", { detail: { message, tone } }));
-  } catch {
-    // Ignore event dispatch errors.
-  }
-};
